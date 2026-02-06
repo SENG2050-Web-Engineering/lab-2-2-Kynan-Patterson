8a6a0f458cb2278804b6f5ab14988b510a526f02
Kynan Patterson <c3527550@uon.edu.au>
2026-02-06 16:26:58 +1100

    testing terminal commands

--- lab2/src/main/java/seng2050/lab2/SimpleMessage.java
+++ lab2/src/main/java/seng2050/lab2/SimpleMessage.java
@@ -10,13 +10,13 @@ import jakarta.servlet.http.HttpServletResponse;
 public class SimpleMessage extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
         throws ServletException, IOException {
-            
+
         PrintWriter out = resp.getWriter();
         out.println("<!DOCTYPE html>");
         out.println("<html>");
-        out.println("<head> <title> Hello </title> </head>");
+        out.println("<head> <title> s1mpel </title> </head>");
         out.println("<body>");
-        out.println("\t<h1> Hello World! (from Hello World Servlet)</h1>");
+        out.println("\t<h1> yerp! (from simple message servlet lets go)</h1>");
         out.println("</body>");
         out.println("</html>");
     }
